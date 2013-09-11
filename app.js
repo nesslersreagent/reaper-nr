5a33398e09aabfd8590cb8ed86e60b61da8d22b9
OpenShift guest <nesslersreagent@gmail.com>
2013-09-11 08:15:01 -0400

    Made changes to latest log date

--- app.js
+++ app.js
@@ -7,52 +7,49 @@ process.env.TZ = 'IST';
 var ip = process.env.IP;
 var port = process.env.PORT;
 var app_path = __dirname;
-var app_uri = 'http://nodejsworkspace.nesslersreagent.c9.io/';
+var app_uri = 'http://reaper-nr.ap01.aws.af.cm/';
 
 var db_reaper = mongo.db('mongodb://reaper-ng:mZhRgLAa4b@ds037488.mongolab.com:37488/reaper-ng');
 var coll_reaper_data = db_reaper.collection('reaper-ng-data');
 
+var latest_log_date = new Date(01,01,01);
+
 function reaper_status(req, res){
-  var num_of_logs = 10;
-  var latest_log_date = new Date();
+  var num_of_logs = 0;
 
   coll_reaper_data.count(function (err, num_of_docs) {
     num_of_logs = num_of_docs;
     coll_reaper_data.find( {}, {'date': true, 'D.T': true, 'D.S': true, 'D.N': true} ).toArray(function (err, data) {
       //var i_data = data.length - 1;
       //var i_data_D = data[i_data].D.length - 1;
-      latest_log_date = data[data.length-1].date.toLocaleDateString();
+      for(var i=0;i<=data.length-1;i++){
+          if(data[i].date > latest_log_date) latest_log_date = data[i].date;
+      }
+      var latest_log_date_string = latest_log_date.toLocaleDateString();
       //console.log('Latest entry: ');
       //console.log( data[i_data].D[i_data_D].N.toString() + ' ' + data[i_data].D[i_data_D].S.toString() + ' ' + data[i_data].D[i_data_D].T.toISOString()  );
-      res.render('reaper-nr', {num_of_logs: num_of_logs, latest_log_date: latest_log_date});  
+      res.render('reaper-nr', {num_of_logs: num_of_logs, latest_log_date: latest_log_date_string});  
     });
   });
 }
 
 function log_file_download(req, res){
-  var latest_log_date = new Date();
-  var i_data = 0;
-  var i_data_D = 0;
   var log_data = '';
   var file = app_path + '/public/logs/log.txt';
   
-  coll_reaper_data.find( {}, {'date': true, 'D': true} ).toArray(function (err, data) {
-    i_data = data.length - 1;
-    i_data_D = data[i_data].D.length - 1;
-    latest_log_date = data[i_data].date;
+  fs.writeFileSync(file, '[ REAPER LOG FILE Dated: ' + latest_log_date.toLocaleDateString() + ' ]\r\n\r\n', 'utf-8');
 
-    fs.writeFileSync(file, '[ REAPER LOG FILE Dated: ' + latest_log_date.toLocaleDateString() + ' ]\r\n\r\n', 'utf-8');
-
-    coll_reaper_data.findOne( { 'date': latest_log_date }, {'D.T': true, 'D.S': true, 'D.N': true}, function (err, data) {
-      for (var i=0; i<=i_data_D; i++){
-        //console.log( data.D[i].T.toISOString() + ' ' + data.D[i].S.toString() + ' ' + data.D[i].N.toString() );  
-        log_data = data.D[i].T.toISOString().substr(11,12) + ' ' + data.D[i].S.toString() + ' ' + data.D[i].N.toString() + '\r\n';
-        fs.appendFileSync(file, log_data, 'utf-8');    
+  coll_reaper_data.findOne( {'date': latest_log_date}, {'D.T': true, 'D.S': true, 'D.N': true}, function (err, data) {
+      var i_data = data.D.length-1;
+      for (var i=0; i<=i_data; i++){
+          //console.log( data.D.T.toISOString() + ' ' + data.D.S.toString() + ' ' + data.D.N.toString() );  
+          log_data = data.D[i].T.toISOString().substr(11,12) + ' ' + data.D[i].S.toString() + ' ' + data.D[i].N.toString() + '\r\n';
+          fs.appendFileSync(file, log_data, 'utf-8');    
       }
       res.download(file);
-    });
   });
 }
+
 
 var app = express();
 
